a73802ecef1bd4ecb9218d9739de6d4a91ad7fef
mehdi parsaei <mehdiparsaeigm@gmail.com>
2021-01-01 13:22:11 +0330

    Initial Commit

---
+++ app/src/main/java/mp/parsa/mvvmtododb/MainActivity.kt
@@ -0,0 +1,11 @@
+package mp.parsa.mvvmtododb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
